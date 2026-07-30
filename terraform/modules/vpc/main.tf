2e1e58a14294f26baf2a5cc238a0bebfe2d3a037
Gianmarco Linares Becerra <devgm1995@outlook.com>
2026-07-30 18:45:23 -0500

    feat(terraform): provision networking infrastructure

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -25,7 +25,8 @@ resource "aws_subnet" "public" {
     map_public_ip_on_launch = true
 
     tags = {
-        Name = "${var.name}-public-${var.availability_zones[count.index]}"
+        Name                     = "${var.name}-public-${var.availability_zones[count.index]}"
+        "kubernetes.io/role/elb" = "1"
     }
 }
 
@@ -37,7 +38,8 @@ resource "aws_subnet" "private" {
     availability_zone = var.availability_zones[count.index]
 
     tags = {
-        Name = "${var.name}-private-${var.availability_zones[count.index]}"
+        Name                              = "${var.name}-private-${var.availability_zones[count.index]}"
+        "kubernetes.io/role/internal-elb" = "1"
     }
 }
 
@@ -59,4 +61,45 @@ resource "aws_route_table_association" "public" {
 
     subnet_id      = aws_subnet.public[count.index].id
     route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+    domain = "vpc"
+
+    tags = {
+        Name = "${var.name}-nat-eip"
+    }
+}
+
+resource "aws_nat_gateway" "this" {
+    allocation_id = aws_eip.nat.id
+    subnet_id     = aws_subnet.public[0].id
+
+    depends_on = [
+        aws_internet_gateway.this
+    ]
+
+    tags = {
+        Name = "${var.name}-nat"
+    }
+}
+
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.this.id
+
+    route {
+        cidr_block     = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.this.id
+    }
+
+    tags = {
+        Name = "${var.name}-private-rt"
+    }
+}
+
+resource "aws_route_table_association" "private" {
+    count = length(aws_subnet.private)
+
+    subnet_id      = aws_subnet.private[count.index].id
+    route_table_id = aws_route_table.private.id
 }
